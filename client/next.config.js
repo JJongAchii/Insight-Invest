e497565eb4662bb72bae735ad21cca9a16489d31
Jong Hyun Park <tony5769@naver.com>
2024-11-06 14:48:50 +0900

    [Refactor] Config setting

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // 빌드 시 ESLint 오류 무시
+  },
+  /* 다른 설정 옵션들 */
 };
 
 module.exports = nextConfig;
