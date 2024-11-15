500557f85491434a558467818673bb727663b67c
Jong Hyun Park <tony5769@naver.com>
2024-11-15 10:31:54 +0900

    [Fix] Test

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# cron을 백그라운드에서 실행
-cron &
+# Start cron in the foreground to avoid PID file creation
+cron -f &
 
-# uvicorn 서버를 비루트 사용자로 실행
-runuser -u appuser -- uvicorn app.main:app --host 0.0.0.0 --port 8000
+# Start uvicorn
+uvicorn app.main:app --host 0.0.0.0 --port 8000
