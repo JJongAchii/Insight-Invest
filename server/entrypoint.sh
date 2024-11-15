60030844f5db297e79922eb1a43f1cbc1d1a737f
Jong Hyun Park <tony5769@naver.com>
2024-11-15 10:40:40 +0900

    [Fix] Test

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Start cron in the foreground to avoid PID file creation
-cron -f &
+# Start cron with custom PID file location
+cron -f -L 15 -p /tmp/cron.pid &
 
 # Start uvicorn
 uvicorn app.main:app --host 0.0.0.0 --port 8000
