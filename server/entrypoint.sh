8eba59459c9cfaaf1d0c18115eb79d7f9c560aaf
Jong Hyun Park <tony5769@naver.com>
2024-11-15 10:09:34 +0900

    [Fix] TEst

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # cron을 백그라운드에서 실행하며, PID 파일을 /tmp에 생성하도록 설정
-cron -L 15 -f -p /tmp/crond.pid &
+cron -L 15 -f /tmp/crond.pid &
 
 # uvicorn 서버 실행
 uvicorn app.main:app --host 0.0.0.0 --port 8000
