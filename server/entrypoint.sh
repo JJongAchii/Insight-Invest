8735ec31582052c9a34fd3b89c16b05e5afc01c2
Jong Hyun Park <tony5769@naver.com>
2024-11-15 09:46:04 +0900

    [Fix] Test

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Start cron in the background as root
-cron -f &
+# cron을 루트 권한으로 실행
+sudo cron -f &
 
-# Start uvicorn
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+# uvicorn을 비루트 사용자로 실행
+sudo -u appuser uvicorn app.main:app --host 0.0.0.0 --port 8000
